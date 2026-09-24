90a8888677516d18a2728ba6ca9010e3590e1b0d
HyunSic <ditto0310@gmail.com>
2026-09-24 23:34:21 +0900

    db: backfill notice table role revokes

---
+++ drizzle/0004b_notices_role_revokes.sql
@@ -0,0 +1,3 @@
+revoke all on prayer_app.notices from anon, authenticated;
+revoke all on prayer_app.notice_reads from anon, authenticated;
+revoke all on prayer_app.push_subscriptions from anon, authenticated;
