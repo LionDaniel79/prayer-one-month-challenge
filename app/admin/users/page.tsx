bcece31cf3ecc7da8787542c34639f0948cc61b0
HyunSic <ditto0310@gmail.com>
2026-09-26 14:37:47 +0900

    refactor: split prayer and user admin management

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,9 @@
+import { AdminUserManagementLoader } from "../../../components/admin/users/AdminUserManagementLoader";
+
+export default function AdminUsersPage() {
+  return (
+    <main className="admin-shell">
+      <AdminUserManagementLoader />
+    </main>
+  );
+}
